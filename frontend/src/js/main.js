09e7dea4a97e6874f15f8f7d73d9fc8495819159
stewartrowley <stewy.rowley@gmail.com>
2022-11-29 20:31:34 -0700

    added a little code

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,5 +1,12 @@
 import '../css/style.css';
 
-import { loadHeaderFooter } from './uitils';
+// import { loadHeaderFooter } from './uitils';
 
-loadHeaderFooter();
+// loadHeaderFooter();
+
+fetch('https://artportfolio.onrender.com/api/getAll') 
+.then((response) => {
+  console.log(response)  
+}) .then((error) => {
+    console.log(error)
+});
