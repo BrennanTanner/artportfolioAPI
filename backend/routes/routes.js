1cbf743a19c100e950c1b9b43bf91070712ba238
Brennan Tanner <tan18010@byui.edu>
2022-12-02 18:11:57 -0700

    removed some redundant consolelogs

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,15 +4,6 @@ const Model = require('../models/model');
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-
-if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
-   console.warn('!! cloudinary config is undefined !!');
-   console.warn('export CLOUDINARY_URL or set dotenv file');
- } else {
-   console.log('cloudinary config:');
-   console.log(cloudinary.config());
- }
-
 router.use(express.json());
 
 cloudinary.config({
