f5e8a6a3c06e39013c3fe6e4ee684364719b79cb
Brennan Tanner <tan18010@byui.edu>
2022-12-02 18:37:37 -0700

    ok fixed the cloudinary config

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -10,8 +10,7 @@ router.use(express.json());
 cloudinary.config({
    cloud_name: process.env.CLOUD_NAME,
    api_key: process.env.API_KEY,
-   api_secret: process.env.API_SECRET, 
-   secure: true
+   api_secret: process.env.API_SECRET
  });
 
 //new user method
